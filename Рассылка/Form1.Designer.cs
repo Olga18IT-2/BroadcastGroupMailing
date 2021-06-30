a90b174ff617c7d680087e1e9eda83f1c09dfd03
Olga Chukova <Olga Chukova@LAPTOP-OF-OLGA>
2021-07-01 01:02:07 +0300

    Добавьте файлы проекта.

---
+++ Рассылка/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Рассылка
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.textMessages = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 25.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Name :";
+            // 
+            // textName
+            // 
+            this.textName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textName.Location = new System.Drawing.Point(180, 33);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(267, 38);
+            this.textName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 25.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(453, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = " Write your message :";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonStart.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(47, 203);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(140, 35);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(211, 203);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(140, 35);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSend.Location = new System.Drawing.Point(807, 88);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(140, 35);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = false;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(462, 131);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(485, 107);
+            this.textMessage.TabIndex = 6;
+            // 
+            // textMessages
+            // 
+            this.textMessages.Location = new System.Drawing.Point(47, 254);
+            this.textMessages.Multiline = true;
+            this.textMessages.Name = "textMessages";
+            this.textMessages.ReadOnly = true;
+            this.textMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textMessages.Size = new System.Drawing.Size(900, 322);
+            this.textMessages.TabIndex = 7;
+            this.textMessages.TextChanged += new System.EventHandler(this.textMessages_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 597);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(999, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(999, 619);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textMessages);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Name = "ChatForm";
+            this.Text = "Chat";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.TextBox textMessages;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+    }
+}
+
---
+++ Рассылка/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Рассылка
+{
+    public partial class ChatForm : Form
+    {
+        private bool done = true;
+        private UdpClient client;
+        private IPAddress groupAddress;
+        private int localPort;
+        private int remotePort;
+        private int ttl;
+        private IPEndPoint remoteEP;
+        private UnicodeEncoding encoding = new UnicodeEncoding();
+        private string name;
+        private string message;
+
+        public ChatForm()
+        {
+            InitializeComponent();
+            try
+            {
+                //Считываем конфигурационный файл приложения
+                NameValueCollection configuration = ConfigurationSettings.AppSettings;
+                groupAddress = IPAddress.Parse(configuration["groupAddress"]);
+                localPort = int.Parse(configuration["LocalPort"]);
+                remotePort = int.Parse(configuration["RemotePort"]);
+                ttl = int.Parse(configuration["TTL"]);
+            }
+            catch
+            {
+                MessageBox.Show(this, "Ошибка в конфигурационном файле!", "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                buttonStart.Enabled = false;
+            }
+        }
+
+
+        private void StopListener()
+        {   //Отправляем группе сообщение о выходе
+            byte[] data = encoding.GetBytes(name + " покинул чат");
+            client.Send(data, data.Length, remoteEP);
+            //Покидаем группу
+            client.DropMulticastGroup(groupAddress);
+            client.Close();
+            //Останавливаем поток, получающий сообщения
+            done = true;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonSend.Enabled = false;
+        }
+
+        private void DisplayReceivedMessage()
+        {
+            string time = DateTime.Now.ToString("t");
+            textMessages.Text = time + " " + message + "\r\n" + textMessages.Text;
+            status.Text = "Последнее сообщение " + time;
+        }
+
+        // Основной метод слушающего потока, который получает данные
+        private void Listener()
+        {   done = false;
+            try
+            {   while (!done)
+                {   IPEndPoint ep = null;
+                    byte[] buffer = client.Receive(ref ep);
+                    message = encoding.GetString(buffer);
+                    this.Invoke(new MethodInvoker(DisplayReceivedMessage));
+                }
+            }
+            catch (Exception ex)
+            {   MessageBox.Show(this, ex.Message, "Ошибка!", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            name = textName.Text;
+            textName.ReadOnly = true;
+            try
+            {   // Присоединяемся к группе рассылки
+                client = new UdpClient(localPort);
+                client.JoinMulticastGroup(groupAddress, ttl);
+                remoteEP = new IPEndPoint(groupAddress, remotePort);
+
+                // Запускаем поток, получающий сообщения
+                Thread receiver = new Thread(new ThreadStart(Listener));
+                receiver.IsBackground = true;
+                receiver.Start();
+                //Отправляем первое сообщение группе
+                byte[] data = encoding.GetBytes("Пользователь " + name + " присоединился к чату");
+                client.Send(data, data.Length, remoteEP);
+
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = true;
+                buttonSend.Enabled = true;
+            }
+            catch (SocketException ex)
+            {   MessageBox.Show(this, ex.Message, "Ошибка", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {   //Отправляем сообщение группе
+                byte[] data = encoding.GetBytes(name + ": " + textMessage.Text);
+                client.Send(data, data.Length, remoteEP);
+                textMessage.Clear();
+                textMessage.Focus();
+            }
+            catch (Exception ex)
+            {   MessageBox.Show(this, ex.Message, "Ошибка!", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            StopListener();
+        }
+
+        private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!done)
+                StopListener();
+        }
+
+        private void textMessages_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
